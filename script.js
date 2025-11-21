748e1ed9efcb730d5cfd85a43c152fee87b6cd20
Shamilshiras <122472013+Shamilshiras@users.noreply.github.com>
2025-11-21 23:34:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function buttonclick(val){
+    document.getElementById("screen").value+=val;
+}
+function clear1(){
+    document.getElementById("screen").value=""
+}
+function equalClick(){
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById("screen").value=result
+}
